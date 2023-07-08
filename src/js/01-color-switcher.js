36f876f8de5a053f1dfab06f5abdcc3ce6c3937f
26Yuliia <124077868+26Yuliia@users.noreply.github.com>
2023-07-08 22:00:32 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  startBtn: document.querySelector('button[data-action="start"'),
+  stopBtn: document.querySelector('button[data-action="stop"'),
+};
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const INTERVAL_DELAY = 1000;
+let intervalId = null;
+
+refs.btnStart.addEventListener('click', changeColor);
+refs.btnStop.addEventListener('click', onBtnStop);
+function changeColor() {
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor =
+      colors[getRandomHexColor(0, colors.length - 1)];
+  }, INTERVAL_DELAY);
+  refs.btnStart.disabled = true;
+}
+
+function onBtnStop() {
+  clearInterval(intervalId);
+  refs.btnStart.disabled = false;
+}
