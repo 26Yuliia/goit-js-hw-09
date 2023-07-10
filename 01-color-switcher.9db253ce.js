c0cee45193e8d5a2a0f00074f759b1546c20b7fd
26Yuliia <124077868+26Yuliia@users.noreply.github.com>
2023-07-10 13:43:45 +0000

    Deploying to gh-pages from @ 26Yuliia/goit-js-hw-09@6d3c3ff328a271213ae674a49ca1569d781560b8 🚀

--- 01-color-switcher.9db253ce.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");e.disabled=!0;let o=null;function r(){n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(function(){o=setInterval(r,1e3)})),e.addEventListener("click",(function(){clearInterval(o)})),e.toggleAttribute("disabled");
-//# sourceMappingURL=01-color-switcher.9db253ce.js.map
